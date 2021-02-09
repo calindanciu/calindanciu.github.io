eb6656794f8861786b2b8e5ec564d6088a7f3708
diversew <diversew@gmail.com>
2021-02-09 01:53:09 +0100

    portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,6 @@
+
 module.exports = {
-  purge: [],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-module.exports = {
-  
   theme: {
     extend: {
       minHeight: {
